895fce5ceeadfd89157dbb76d7416f1e67ffc132
nikola <savicn209@gmail.com>
2023-08-22 23:36:17 +0200

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    console.log('setup');
+
+    let name = 'Nikola'
+    let age = 26
+
+    return { name, age }
+  },
 }
 </script>

623716bc05df8fbf2866fbdd03f9edafde5f25cd
nikola <savicn209@gmail.com>
2023-08-23 00:22:50 +0200

    hove view setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,41 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <input type="text" v-model="search">
+    <p>searched term - {{ search }}</p>
+    <div v-for="name in matchinNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
+import { computed, ref, watch, watchEffect } from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
-    console.log('setup');
+    const search = ref('')
+    const names = ref(['nik', 'asdas', 'asd123', 'xcvzxc', 'ghjfgh', 'tyuyrt'])
 
-    let name = 'Nikola'
-    let age = 26
+    watch(search, () => {
+      console.log('watcher');
+    })
 
-    return { name, age }
+    watchEffect(() => {
+      console.log('vacer');
+    })
+
+    const matchinNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    return { names, search, matchinNames }
   },
+  data() {
+    return {
+      score: 5
+    }
+  }
 }
 </script>
